b944a7d8a3b233899ca32fc7252926f8906eb508
Deran Decker <me@derandecker.com>
2019-08-09 06:49:28 -1000

    setup main activity layout. add beginning of code to display and save movies.

---
+++ app/src/main/java/com/derandecker/android_sprint1_challenge/model/Movie.kt
@@ -0,0 +1,6 @@
+package com.derandecker.android_sprint1_challenge.model
+
+class Movie(
+    val title: String,
+    val watched: Boolean
+)
--- app/src/main/java/com/derandecker/android_sprint1_challenge/ui/MainActivity.kt
+++ app/src/main/java/com/derandecker/android_sprint1_challenge/ui/MainActivity.kt
@@ -2,12 +2,37 @@ package com.derandecker.android_sprint1_challenge.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.derandecker.android_sprint1_challenge.R
+import com.derandecker.android_sprint1_challenge.model.Movie
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    var movieList = mutableListOf<Movie>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+//        linear_layout_movie_list.addView(view)
+    }
+
+
+    override fun onPostResume() {
+        refreshMovies()
+        super.onPostResume()
+    }
+
+    fun refreshMovies() {
+        linear_layout_movie_list.removeAllViews()
+        movieList.forEachIndexed { index, movie ->
+            val view: View = createTextView(movie, index)
+            linear_layout_movie_list.addView(view)
+        }
+
     }
 }
+
