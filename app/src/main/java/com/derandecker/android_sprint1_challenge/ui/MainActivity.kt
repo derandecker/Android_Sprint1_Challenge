6cf97467f655634e4bd2988cfe69e81f85f758a9
Deran Decker <me@derandecker.com>
2019-08-09 06:00:30 -1000

    initial commit

---
+++ app/src/main/java/com/derandecker/android_sprint1_challenge/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.derandecker.android_sprint1_challenge.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.derandecker.android_sprint1_challenge.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
