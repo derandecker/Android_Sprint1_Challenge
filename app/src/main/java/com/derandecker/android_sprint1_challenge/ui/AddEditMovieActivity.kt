ae3f019bbce8380ecb74c50685a72e2fd7d69f52
Deran Decker <me@derandecker.com>
2019-08-09 07:48:03 -1000

    Add AddEditMovieActivity xml layout

---
+++ app/src/main/java/com/derandecker/android_sprint1_challenge/ui/AddEditMovieActivity.kt
@@ -0,0 +1,21 @@
+package com.derandecker.android_sprint1_challenge.ui
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import com.derandecker.android_sprint1_challenge.R
+
+import kotlinx.android.synthetic.main.activity_add_edit_movie.*
+
+class AddEditMovieActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_movie)
+        setSupportActionBar(toolbar)
+
+
+        }
+    }
+
+}
